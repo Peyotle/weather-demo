af4bd867159baa76a240eab94def9f94392563b3
Oleg Chernyshenko <oleg@tugboat.nz>
2018-01-19 17:18:03 +1300

    Move MainView to a separate file Add additional stack views for the labels

--- Weather/WeatherViewController.swift
+++ Weather/WeatherViewController.swift
@@ -18,114 +18,13 @@ class WeatherViewController: UIViewController {
     override func loadView() {
         self.view = WeatherView(frame: UIScreen.main.bounds)
     }
-}
 
-class WeatherView: UIView {
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        let isLandscape = size.width > size.height
+        if isLandscape {
+            mainView.centerContentStack.axis = .horizontal
+        } else {
+            mainView.centerContentStack.axis = .vertical
+        }
     }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setup() {
-        setupViews()
-        setupConstraints()
-    }
-
-    private func setupViews() {
-        self.addSubview(backgroundView)
-        self.addSubview(mainStack)
-        mainStack.addArrangedSubview(conditionsImageView)
-        mainStack.addArrangedSubview(temperatureLabel)
-        mainStack.addArrangedSubview(cityLabel)
-        mainStack.addArrangedSubview(conditionsLabel)
-        mainStack.addArrangedSubview(buttonsStack)
-        buttonsStack.addArrangedSubview(celsiusButton)
-        buttonsStack.addArrangedSubview(fahrenheitButton)
-    }
-
-    private func setupConstraints() {
-        backgroundView.pinEdges(to: self)
-        mainStack.pinEdges(to: self)
-    }
-
-    // MARK: - Views
-
-    let backgroundView: UIView = {
-        let view = UIView(frame: .zero)
-        view.backgroundColor = .red
-        return view
-    }()
-
-    let mainStack: UIStackView = {
-        let stackView = UIStackView(frame: .zero)
-        stackView.axis = .vertical
-        stackView.distribution = .equalSpacing
-        stackView.spacing = 10
-        return stackView
-    }()
-
-    let temperatureLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.font = UIFont.systemFont(ofSize: 144)
-        label.textColor = .white
-        label.textAlignment = .center
-        label.text = "18°"
-        return label
-    }()
-
-    let cityLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.font = UIFont.systemFont(ofSize: 36)
-        label.textColor = .white
-        label.textAlignment = .center
-        label.text = "Christchurch"
-        return label
-    }()
-
-    let conditionsLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.font = UIFont.systemFont(ofSize: 20)
-        label.textColor = .white
-        label.textAlignment = .center
-        label.text = "Sunny"
-        return label
-    }()
-
-    let conditionsImageView: UIImageView = {
-        let image = UIImage(named: "sun")
-        let imageView = UIImageView(image: image)
-        imageView.contentMode = .scaleAspectFit
-        imageView.widthAnchor.constraint(equalToConstant: image!.size.width).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: image!.size.height).isActive = true
-        return imageView
-    }()
-
-    let celsiusButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("°C", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 73)
-        button.setTitleColor(.white, for: .normal)
-        return button
-    }()
-
-    let fahrenheitButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("°F", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 73)
-        button.setTitleColor(.white, for: .normal)
-        return button
-    }()
-
-    let buttonsStack: UIStackView = {
-        let stackView = UIStackView(frame: .zero)
-        stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
-        stackView.spacing = 10
-        return stackView
-    }()
 }
